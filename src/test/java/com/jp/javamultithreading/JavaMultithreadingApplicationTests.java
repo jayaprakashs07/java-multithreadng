8c613c08bed2e59efcae5ea91d4cc3eeed01176c
Jayaprakash S <jp.jayaprakash779@gmail.com>
2022-01-16 12:18:29 +0530

    project setup

---
+++ src/test/java/com/jp/javamultithreading/JavaMultithreadingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jp.javamultithreading;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaMultithreadingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
